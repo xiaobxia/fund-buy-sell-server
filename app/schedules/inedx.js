59ffe07279e43b862856a4136833c9af0d354a55
xiaobxia <673806687@qq.com>
2019-07-01 10:58:05 +0800

    xg

--- app/schedules/inedx.js
+++ app/schedules/inedx.js
@@ -2,11 +2,11 @@
 // const isDev = env === 'dev'
 exports.marketOpen = require('./marketOpen')
 exports.openWork = require('./openWork')
-exports.updateCustomerTodayHistory = require('./updateCustomerTodayHistory')
+// exports.updateCustomerTodayHistory = require('./updateCustomerTodayHistory')
 exports.updateFixedInvestment = require('./updateFixedInvestment')
 exports.updateBand = require('./updateBand')
 exports.updateMonthRate = require('./updateMonthRate')
-exports.clearToday = require('./clearToday')
-exports.updateCustomerCanUseDay = require('./updateCustomerCanUseDay')
-
-exports.appointed = require('./appointed')
+// exports.clearToday = require('./clearToday')
+// exports.updateCustomerCanUseDay = require('./updateCustomerCanUseDay')
+//
+// exports.appointed = require('./appointed')
