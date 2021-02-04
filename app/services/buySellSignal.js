cb5b35cbc70d4179e62a979afc6cac1bc96458dc
xiaobxia <673806687@qq.com>
2021-02-04 15:57:49 +0800

    xg

--- app/services/buySellSignal.js
+++ app/services/buySellSignal.js
@@ -16,7 +16,7 @@ exports.updateSignal = async function (data) {
     updateData.band_record = JSON.parse(data.band_record)
   }
   if (data.position) {
-    updateData.band_record = data.position
+    updateData.position = data.position
   }
   const buySellSignal = await BuySellSignalProxy.findOne({
     trade_date: data.trade_date
